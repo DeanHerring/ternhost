08b7eabe6e3a5a1fe8e3fc5b249db6e86be63088
0xGod <ddak1914@gmail.com>
2024-01-31 16:43:03 +0200

    feat: made a section with an accordion

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Slider
 var swiper = new Swiper('.mySwiper', {
   slidesPerView: 3,
   spaceBetween: 50,
@@ -17,3 +18,25 @@ var swiper = new Swiper('.mySwiper', {
     },
   },
 });
+
+console.log('hello, world');
+
+// Accordion
+
+// TODO: Super-Mega-Duper-Puper-Ultra-Alpha-Beta-Gamma Shitcode
+const accordions = document.querySelectorAll('.accordion');
+
+const initAccordions = (collection) => {
+  collection.forEach((accordion) => {
+    accordion.addEventListener('click', (e) => {
+      const target = e.target;
+      const node = target.closest('.accordion');
+      const arrow = target.querySelector('.accordion__head-switch');
+
+      node.classList.toggle('show');
+      arrow.classList.toggle('reverse');
+    });
+  });
+};
+
+initAccordions(accordions);
